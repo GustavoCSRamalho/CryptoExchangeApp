bbc6f27717484c91887db4e35ba3f5fd626a9f3c
Gustavo de Camargo da Silva Ramalho <gustavocsramalho@gmail.com>
2025-11-24 20:02:09 -0300

    Initial Commit

---
+++ CryptoExchangeApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CryptoExchangeApp
+//
+//  Created by Gustavo Ramalho on 24/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CryptoExchangeApp/CryptoExchangeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoExchangeAppApp.swift
+//  CryptoExchangeApp
+//
+//  Created by Gustavo Ramalho on 24/11/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoExchangeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
